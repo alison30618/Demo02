d2592994061cebff605b906388fea3af277623a5
alison angulo diosa <alisonangulodiosa@gmail.com>
2024-10-16 12:02:00 -0500

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyLibrary;
+using static MyLibrary.Employee;
+
+Secretary Maria = new Secretary();
+Maria.Name = "Maria";
+Manager Pedro = new Manager();
+Pedro.Name = "Pedro";
+Developer Miguel = new Developer();
+Miguel.Name = "Miguel";
+Console.WriteLine($"Dias: {Miguel.GetPaymentDays()}");
+
+WriteEmployeeinfo(Maria);
+WriteEmployeeinfo(Pedro);
+WriteEmployeeinfo(Miguel);
+
+void WriteEmployeeinfo(Employee employee)
+{
+    Console.WriteLine($"{ employee.Name}: {employee.CalculatorMonthlyPayment()}");
+    Console.WriteLine($"Days:  {employee.GetPaymentDats()}");
+}
---
+++ MyLibrary/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int GetInt()
+        {
+            return 5;
+        }
+
+        public virtual decimal CalculatorMonthlyPayment()
+        {
+            return 12000;
+        }
+
+        public int GetPaymentDats()
+        {
+            return 30;
+        }
+
+    }
+
+        public class Secretary : Employee
+        {
+            public override decimal CalculatorMonthlyPayment() => 15000;
+
+        }
+
+        public class Manager : Employee
+        {
+            public override decimal CalculatorMonthlyPayment()
+            {
+                return 30000;
+            }
+        }
+
+        public class Developer : Employee
+        {
+         public new int GetPaymentDays()
+        {
+            return 20;
+        }
+        }
+    
+}
